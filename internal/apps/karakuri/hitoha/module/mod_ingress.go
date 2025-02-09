21cc59daff117acc28e3fcfa3809380fe83ea0db
pyxgun <jixeis9541@gmail.com>
2025-02-09 11:09:14 +0000

    improve ingress certificate parameter

--- internal/apps/karakuri/hitoha/module/mod_ingress.go
+++ internal/apps/karakuri/hitoha/module/mod_ingress.go
@@ -1,7 +1,9 @@
 package karakuri_mod
 
 import (
+	"errors"
 	"karakuripkgs"
+	"net"
 	"os"
 	"os/exec"
 	"strings"
@@ -12,14 +14,26 @@ func createIngressModuleKarakurifile() {
 	karakurifile := `FROM nginx:alpine
 
 COPY /etc/karakuri/modules/ingress/script /script
-COPY /etc/karakuri/modules/ingress/cert /cert
+COPY /etc/karakuri/modules/ingress/certconf /certconf
 COPY /etc/karakuri/modules/ingress/conf /conf
 
-RUN apk update
-RUN apk add openssl
-RUN openssl genrsa 2048 > /cert/server.key
-RUN openssl req -new -key /cert/server.key -config /cert/ssl.cnf -out /cert/server.csr
-RUN openssl x509 -req -days 3650 -in /cert/server.csr -signkey /cert/server.key -out /cert/server.crt -extfile /cert/san.txt
+RUN apk update && apk add openssl
+
+RUN mkdir rootca interca server
+RUN openssl genrsa -out rootca/rootca.key
+RUN openssl req -new -key rootca/rootca.key -out rootca/rootca.csr -subj "/C=JP/ST=Tokyo/CN=Karakuri Ingress RootCA"
+RUN openssl x509 -req -in rootca/rootca.csr -signkey rootca/rootca.key -days 365 -sha256 -extfile /certconf/ca_v3.ext -out rootca/rootca.crt
+
+RUN openssl genrsa -out interca/interca.key
+RUN openssl req -new -key interca/interca.key -out interca/interca.csr -subj "/C=JP/ST=Tokyo/CN=Karakuri Ingress InterCA"
+RUN openssl x509 -req -in interca/interca.csr -CA rootca/rootca.crt -CAkey rootca/rootca.key -CAcreateserial -days 365 -sha256 -out interca/interca.crt -extfile /certconf/ca_v3.ext
+
+RUN openssl genrsa -out server/server.key
+RUN openssl req -new -key server/server.key -out server/server.csr -subj "/C=JP/ST=Tokyo/CN=Karakuri Ingress Host"
+RUN openssl x509 -req -in server/server.csr -CA interca/interca.crt -CAkey interca/interca.key -CAcreateserial -days 365 -sha256 -out server/server.crt -extfile /certconf/server_v3.ext
+
+RUN cat server/server.crt interca/interca.crt rootca/rootca.crt > server/chain.crt
+
 RUN cp /conf/server.conf /etc/nginx/conf.d/
 
 CMD ["sh", "/script/entrypoint.sh"]
@@ -38,6 +52,54 @@ CMD ["sh", "/script/entrypoint.sh"]
 	}
 }
 
+func isPrivateIP(ip net.IP) bool {
+	var prvMasks []*net.IPNet
+
+	for _, cidr := range []string{
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+	} {
+		_, mask, _ := net.ParseCIDR(cidr)
+		prvMasks = append(prvMasks, mask)
+	}
+
+	for _, mask := range prvMasks {
+		if mask.Contains(ip) {
+			return true
+		}
+	}
+	return false
+}
+
+func getDeviceIpAddress() (net.IP, error) {
+	ift, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ifi := range ift {
+		addrs, err := ifi.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if isPrivateIP(ip) {
+				return ip, nil
+			}
+		}
+	}
+
+	return nil, errors.New("no IP")
+}
+
 func createIngressModuleConfig() {
 	// conf
 	if _, stat := os.Stat(karakuripkgs.KARAKURI_MOD_INGRESS + "/conf"); stat != nil {
@@ -50,8 +112,8 @@ func createIngressModuleConfig() {
     server_name    ingress.karakuri.container;
 
     listen 443 ssl;
-    ssl_certificate /cert/server.crt;
-    ssl_certificate_key /cert/server.key;
+    ssl_certificate /server/chain.crt;
+    ssl_certificate_key /server/server.key;
 
     client_max_body_size 5G;
     keepalive_timeout  130;
@@ -75,45 +137,51 @@ func createIngressModuleConfig() {
 	}
 
 	// cert
-	if _, stat := os.Stat(karakuripkgs.KARAKURI_MOD_INGRESS + "/cert"); stat != nil {
-		if err := os.MkdirAll(karakuripkgs.KARAKURI_MOD_INGRESS+"/cert", os.ModePerm); err != nil {
+	if _, stat := os.Stat(karakuripkgs.KARAKURI_MOD_INGRESS + "/certconf"); stat != nil {
+		if err := os.MkdirAll(karakuripkgs.KARAKURI_MOD_INGRESS+"/certconf", os.ModePerm); err != nil {
 			panic(err)
 		}
 	}
 
-	ssl_conf := `[ req ]
-default_bits       = 2048
-distinguished_name = req_distinguished_name
-req_extensions     = req_ext
-prompt = no
-[ req_distinguished_name ]
-C = JP
-CN = karakuri ingress
-[ req_ext ]
-subjectAltName = @alt_names
-[ alt_names ]
-DNS.1 = *.karakuri.container
+	device_address, err := getDeviceIpAddress()
+	if err != nil {
+		panic(err)
+	}
+	ca_v3 := `basicConstraints=critical, CA:true
+authorityKeyIdentifier=keyid:always,issuer
+subjectKeyIdentifier=hash
+extendedKeyUsage=serverAuth, clientAuth
+keyUsage=keyCertSign,cRLSign
 `
-	san_txt := "subjectAltName = DNS:*.karakuri.container"
+	server_v3 := `authorityKeyIdentifier=keyid,issuer
+basicConstraints=critical, CA:FALSE
+keyUsage=digitalSignature, keyEncipherment
+extendedKeyUsage=serverAuth, clientAuth
+subjectAltName=@alt_names
 
-	if _, stat := os.Stat(karakuripkgs.KARAKURI_MOD_INGRESS + "/cert/ssl.cnf"); stat != nil {
-		fd_2, err := os.Create(karakuripkgs.KARAKURI_MOD_INGRESS + "/cert/ssl.cnf")
+[alt_names]
+DNS.1 = karakuri.container
+DNS.2 = *.karakuri.container
+IP.1 = ` + device_address.String() + "\n"
+
+	if _, stat := os.Stat(karakuripkgs.KARAKURI_MOD_INGRESS + "/certconf/ca_v3.ext"); stat != nil {
+		fd_2, err := os.Create(karakuripkgs.KARAKURI_MOD_INGRESS + "/certconf/ca_v3.ext")
 		if err != nil {
 			panic(err)
 		}
 		defer fd_2.Close()
-		bytes_2 := []byte(ssl_conf)
+		bytes_2 := []byte(ca_v3)
 		if _, err := fd_2.Write(bytes_2); err != nil {
 			panic(err)
 		}
 	}
-	if _, stat := os.Stat(karakuripkgs.KARAKURI_MOD_INGRESS + "/cert/san.txt"); stat != nil {
-		fd_3, err := os.Create(karakuripkgs.KARAKURI_MOD_INGRESS + "/cert/san.txt")
+	if _, stat := os.Stat(karakuripkgs.KARAKURI_MOD_INGRESS + "/certconf/server_v3.ext"); stat != nil {
+		fd_3, err := os.Create(karakuripkgs.KARAKURI_MOD_INGRESS + "/certconf/server_v3.ext")
 		if err != nil {
 			panic(err)
 		}
 		defer fd_3.Close()
-		bytes_3 := []byte(san_txt)
+		bytes_3 := []byte(server_v3)
 		if _, err := fd_3.Write(bytes_3); err != nil {
 			panic(err)
 		}
